8df881a3de093e64a71f8e651617bf95b21a3760
VINAYAK <VINAYAK@LAPTOP-KKRNRDKL>
2017-01-25 08:14:50 -0500

    GitHub Integration

--- src/hello_github/Hello_GitHub.java
+++ src/hello_github/Hello_GitHub.java
@@ -7,6 +7,8 @@ public class Hello_GitHub {
     
     public static void main(String[] args) {
         System.out.println("Hello World! This is my GitHub account: VinayakIND");
+        System.out.print("I am loving it");
+    
     }
     
 }
